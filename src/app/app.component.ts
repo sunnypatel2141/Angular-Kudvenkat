8f21f43827edf78c04b2771a8cc044218ea272f6
sunnypatel2141 <sanipatel2141@hotmail.com>
2017-10-23 20:51:21 -0400

    Image added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template:`<div>
-                <h1>{{pageHeader}}</h1>
+                <h1>{{getFullName()}}</h1>
+                <img src='{{imagePath}}'/>
                 <app-employee></app-employee>
             </div>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  pageHeader: String = 'Employee Details'
+  pageHeader: string = 'Employee Details';
+  imagePath: string = 'https://i0.wp.com/tentulogo.com/wp-content/uploads/ibm.png';
+
+  firstName: string = 'Sunny';
+  lastName: string = 'Patel';
+
+  getFullName(): string
+  {
+    return this.firstName + ' ' + this.lastName;
+  }
 }
